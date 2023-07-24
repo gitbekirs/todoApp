55f6cd1c6371e912e9cb125752dbf34b594a6ae0
Bekir ŞENGÖNÜL <Bekir.Sengonul@gsb.gov.tr>
2023-07-24 14:45:51 +0300

    Lesson 3: Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'TodoApp';
   //sayfada yapılacak işler ve veri çekme işlemleri vs.. sayfayı dinamik hale getirmek için yapılacaklar burada 
+  user = 'Çınar';
+  items = [
+    { description: "Kahvaltı", action: "No" },
+    { description: "Sinema", action: "No" },
+    { description: "Spor", action: "No" },
+    { description: "Ders Çalışma", action: "No" },
+    { description: "Alışveriş", action: "No" },
+  ];
 }
